0f9426faa3a4fd0063589940092b3b88507d479d
Camilo Agudelo <camiiloagudelo92@gmail.com>
2026-02-18 22:28:47 -0500

    chore: Remove unused export for info_chip.dart from item_card.dart

--- lib/features/maintenance/presentation/widgets/item_card/item_card.dart
+++ lib/features/maintenance/presentation/widgets/item_card/item_card.dart
@@ -3,4 +3,3 @@ export 'maintenance_dates_section.dart';
 export 'maintenance_mileage_info.dart';
 export 'maintenance_notes_section.dart';
 export 'modern_maintenance_card.dart';
-export 'info_chip.dart';
