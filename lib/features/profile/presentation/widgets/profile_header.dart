6607beee444e1ab0139116b8802743a0545c2e8f
Camilo Agudelo <camiiloagudelo92@gmail.com>
2026-06-19 18:51:56 -0500

    Remove edit profile button

--- lib/features/profile/presentation/widgets/profile_header.dart
+++ lib/features/profile/presentation/widgets/profile_header.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:rideglory/core/extensions/l10n_extensions.dart';
 import 'package:rideglory/core/utils/initials.dart';
 import 'package:rideglory/design_system/foundation/theme/app_colors.dart';
 import 'package:rideglory/features/profile/presentation/widgets/profile_avatar.dart';
 import 'package:rideglory/features/users/domain/model/user_model.dart';
-import 'package:rideglory/shared/router/app_routes.dart';
 
 class ProfileHeader extends StatelessWidget {
   const ProfileHeader({super.key, required this.user});
@@ -61,29 +58,6 @@ class ProfileHeader extends StatelessWidget {
             ],
           ),
         ],
-        const SizedBox(height: 16),
-        GestureDetector(
-          onTap: () => context.pushNamed(
-            AppRoutes.editProfile,
-            extra: user,
-          ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            decoration: BoxDecoration(
-              color: AppColors.darkCard,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: AppColors.darkBorderPrimary),
-            ),
-            child: Text(
-              context.l10n.profile_editInfo,
-              style: const TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.w500,
-                color: AppColors.textOnDarkPrimary,
-              ),
-            ),
-          ),
-        ),
       ],
     );
   }
